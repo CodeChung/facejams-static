3272ab7d3867de04865a32fa2226188525ba828d
Harry Chung <hc9825@gmail.com>
2019-08-12 23:26:27 -0700

    Updates

---
+++ precache-manifest.3d88d22d86f88c7a174a9e50b6428c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adca17f79d7fc689d8b644f92c27697a",
+    "url": "/facejams-static/index.html"
+  },
+  {
+    "revision": "f644d12dcd8d8b7765c6",
+    "url": "/facejams-static/static/css/2.f0405d8a.chunk.css"
+  },
+  {
+    "revision": "6bf9dfc92e8fe44980f7",
+    "url": "/facejams-static/static/css/main.2cd954bd.chunk.css"
+  },
+  {
+    "revision": "f644d12dcd8d8b7765c6",
+    "url": "/facejams-static/static/js/2.9274b0d7.chunk.js"
+  },
+  {
+    "revision": "6bf9dfc92e8fe44980f7",
+    "url": "/facejams-static/static/js/main.2c0e3bb2.chunk.js"
+  },
+  {
+    "revision": "c44f5c21a1e734596156",
+    "url": "/facejams-static/static/js/runtime~main.795e7a19.js"
+  }
+]);
